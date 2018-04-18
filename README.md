939956df2432a3ab1bedbfe5e6022f5d5b99acea
Marin <marin.dom64@gmail.com>
2018-04-18 14:54:42 +0200

    visual changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PanelAng
+# AVPanel
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
 
--- src/app/mock-todos.ts
+++ src/app/mock-todos.ts
@@ -1,11 +1,11 @@
 import { Todo } from './todo';
 
 export const TODOS: Todo[] = [
-    {task: "bla", by: "na"},
+    {task: "Set priorities and decide which jobs should be done first.", by: "Random site"},
+    {task: "Focus on tasks that have to be done, without missing any details.", by: "The same site"},
     {task: "rwea", by: "feq"},
     {task: "fsxa", by: "nfefe"},
     {task: "htjya", by: "nvxcv"},
     {task: "gwea", by: "gege"},
     {task: "gsdva", by: "geg"},
-    {task: "bbgw", by: "da"},
 ];
